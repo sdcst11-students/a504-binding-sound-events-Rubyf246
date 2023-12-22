1029487073a23a90ca95803b101ccf67d56de91b
Rubyf246 <144410608+Rubyf246@users.noreply.github.com>
2023-12-21 20:02:45 -0800

    sdftgyhujik

--- task2.py
+++ task2.py
@@ -17,14 +17,96 @@ Keep score for the player and play appropriate sound effects when buttons are pr
 
 """
 window = tk.Tk()
-window.title("my soundboard")
+window.title("Math Drills")
 window.attributes('-topmost',True)
+eoutput = IntVar()
+eoutput.set(0)
+
+def click1(event): 
+    get1=onee.get()
+    get1=float(get1)
+    if get1 == 18:
+        playsound("Tada.wav", block = False)
+    else:
+        playsound("Toy Honk.wav", block = False)
+        
+def click2(event): 
+    get2=twoe.get()
+    get2=float(get2)
+    if get2 == 5:
+        playsound("Tada.wav", block = False)
+    else:
+        playsound("Toy Honk.wav", block = False)
+        
+def click3(event): 
+    get3=threee.get()
+    get3=float(get3)
+    if get3 == 8:
+        playsound("Tada.wav", block = False)
+    else:
+        playsound("Toy Honk.wav", block = False)
+        
+def click4(event): 
+    get4=foure.get()
+    get4=float(get4)
+    if get4 == 6:
+        playsound("Tada.wav", block = False)
+    else:
+        playsound("Toy Honk.wav", block = False)
+        
+def click5(event): 
+    get5=fivee.get()
+    get5=float(get5)
+    if get5 == -16:
+        playsound("Tada.wav", block = False)
+    else:
+        playsound("Toy Honk.wav", block = False)
+        
+def click6(event): 
+    get6=sixe.get()
+    get6=float(get6)
+    if get6 == 20:
+        playsound("Tada.wav", block = False)
+    else:
+        playsound("Toy Honk.wav", block = False)
+
+def click7(event): 
+    get7=sevene.get()
+    get7=float(get7)
+    if get7 == -4:
+        playsound("Tada.wav", block = False)
+    else:
+        playsound("Toy Honk.wav", block = False)
+        
+def click8(event): 
+    get8=eighte.get()
+    get8=float(get8)
+    if get8 == 1:
+        playsound("Tada.wav", block = False)
+    else:
+        playsound("Toy Honk.wav", block = False)
+        
+def click9(event): 
+    get9=ninee.get()
+    get9=float(get9)
+    if get9== -12:
+        playsound("Tada.wav", block = False)
+    else:
+        playsound("Toy Honk.wav", block = False)
+        
+def click10(event): 
+    get10=tene.get()
+    get10=float(get10)
+    if get10 == -7:
+        playsound("Tada.wav", block = False)
+    else:
+        playsound("Toy Honk.wav", block = False)
 
 main= tk.Label(window, text = "Problem Solve")
 sub= tk.Label (window, text= "Solving One Step Equations")
 instruct= tk.Label (window, text= "Find Missing Variables")
 score= tk.Label(window, text= "Score =")
-bscore = tk.Entry(window, width= 7)
+bscore = tk.Entry(window, width= 7, textvariable=eoutput)
 
 one= tk.Label(window, text ="1. 26 = 8 + v   =")
 two= tk.Label (window, text="2. 3 + p = 8   =")
@@ -48,22 +130,41 @@ eighte= tk.Entry(window,width =10)
 ninee= tk.Entry(window,width =10)
 tene= tk.Entry(window,width =10)
 
-b1= tk.Button(window,text= "cal")
-b2= tk.Button(window,text= "cal")
-b3= tk.Button(window,text= "cal")
-b4= tk.Button(window,text= "cal")
-b5= tk.Button(window,text= "cal")
-b6= tk.Button(window,text= "cal")
-b7= tk.Button(window,text= "cal")
-b8= tk.Button(window,text= "cal")
-b9= tk.Button(window,text= "cal")
-b10= tk.Button(window,text= "cal")
+b1= tk.Button(window,text= "check")
+b1.bind("<Button>",click1)
 
-main.grid(row = 1, column = 2 )
-sub.grid (row = 2, column = 2)
-instruct.grid (row = 3, column = 2)
-score.grid(row= 4, column= 2)
-bscore.grid(row = 4, column =3 )
+b2= tk.Button(window,text= "check")
+b2.bind("<Button>",click2)
+
+b3= tk.Button(window,text= "check")
+b3.bind("<Button>",click3)
+
+b4= tk.Button(window,text= "check")
+b4.bind("<Button>",click4)
+
+b5= tk.Button(window,text= "check")
+b5.bind("<Button>",click5)
+
+b6= tk.Button(window,text= "check")
+b6.bind("<Button>",click6)
+
+b7= tk.Button(window,text= "check")
+b7.bind("<Button>",click7)
+
+b8= tk.Button(window,text= "check")
+b8.bind("<Button>",click8)
+
+b9= tk.Button(window,text= "check")
+b9.bind("<Button>",click9)
+
+b10= tk.Button(window,text= "check")
+b10.bind("<Button>",click10)
+
+main.grid(row = 1, column = 1 )
+sub.grid (row = 2, column = 1)
+instruct.grid (row = 3, column = 1)
+score.grid(row= 2, column= 2)
+bscore.grid(row = 2, column =3 )
 
 one.grid (row = 5, column= 1)
 two.grid (row = 6, column= 1)
